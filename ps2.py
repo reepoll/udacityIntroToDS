314f440049bd7e3cb38955dc9c39e4e55edc65c0
reepoll <rappoli.chris@gmail.com>
2016-10-24 21:10:28 -0400

    finshing ps2

--- ps2.py
+++ ps2.py
@@ -10,7 +10,7 @@ def num_rainy_days(f_name):
     q = """
     SELECT count(*)
     FROM weather_data wd
-    WHERE wd.rain == 1
+    WHERE wd.rain == 1;
     """
     
     rainy_days = pdsql.sqldf(q.lower(), locals())
@@ -19,6 +19,23 @@ def num_rainy_days(f_name):
 
 print(num_rainy_days(filename))
 
+
+def avg_weekend_temperature(f_name):
+    weather_data = pd.read_csv('./data/' + f_name)
+    
+    q = """
+    SELECT cast(strftime('%w', date) as integer), AVG(meantempi)
+    FROM weather_data
+    WHERE cast(strftime('%w', date) as integer) == 0 OR cast(strftime('%w', date) as integer) == 6
+    GROUP BY cast(strftime('%w', date) as integer)
+    """
+    
+    mean_temp_weekends = pdsql.sqldf(q.lower(), locals())
+    
+    return mean_temp_weekends
+
+    
+print(avg_weekend_temperature(filename))
 
 
 
